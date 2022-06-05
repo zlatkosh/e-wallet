372fd09fa0c84694fa07aa4bb9efef552b6ba497
Zlatko Sharkoski <zlatko.sharkoski@clarivate.com>
2022-06-05 16:00:19 +0200

    #2 Initial project structure

---
+++ server/src/main/java/com/zlatkosh/ewallet/EWalletServerApplication.java
@@ -0,0 +1,21 @@
+package com.zlatkosh.ewallet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class EWalletServerApplication  implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EWalletServerApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Started the e-wallet application...");
+    }
+}
